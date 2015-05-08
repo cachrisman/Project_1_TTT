e92d8ad9e4a1a917fdb30ebd2ff3684d236c1977
cachrisman <charlie.chrisman@gmail.com>
2015-05-08 14:40:48 -0700

    cleaned up html, added jquery; added document on ready to js

--- app.js
+++ app.js
@@ -2,42 +2,47 @@
 
 // Execute this code only AFTER the document is ready
 // Hint: use jQuery's `$(document).ready`
+$(document).on("ready", function() {
 
-  function Game() {
-    //Create a new instance of player 1
-    //this.player1 = ...
+    function Game() {
+        //Create a new instance of player 1
+        //this.player1 = ...
 
-    //Do the same for a player 2
-    //this.player2 = ...
-    
-    //Create the board
-    //this.board = ...
-  }
+        //Do the same for a player 2
+        //this.player2 = ...
 
-  // Remember: prototypes are shared functions between all game instances
-  Game.prototype.nextPlayer = function() {
-    //Switch players
-  };
+        //Create the board
+        //this.board = ...
+    }
 
-  // `Game.prototype.init` kicks off a new game with a board and two players
-  Game.prototype.init = function() {
-    //
-  };
+    // Remember: prototypes are shared functions between all game instances
+    Game.prototype.nextPlayer = function() {
+        //Switch players
+    };
 
-  // A starter Player constructor.
-  function Player(team) {
-    //Is the player X or O?
-    //this.team = ...
-  };
+    // `Game.prototype.init` kicks off a new game with a board and two players
+    Game.prototype.init = function() {
+        //
+    };
 
-  // A starter Board constructor.
-  function Board() {
-    //Tracks the cells of the board instance
-    //this.$cells = ...
+    // A starter Player constructor.
+    function Player(team) {
+        //Is the player X or O?
+        //this.team = ...
+    }
 
-    //Store any other properties that board may have below, such as a reset option
-  };
+    // A starter Board constructor.
+    function Board() {
 
-  // Start the game!
-  var game = new Game();
-  game.init();
+
+        //Tracks the cells of the board instance
+        //this.$cells = ...
+
+        //Store any other properties that board may have below, such as a reset option
+    }
+
+    // Start the game!
+    var game = new Game();
+    game.init();
+
+});
