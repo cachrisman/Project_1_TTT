09a06b89f850a6b598d41635d085f50f74011781
cachrisman <charlie.chrisman@gmail.com>
2015-05-09 13:48:53 -0700

    added turnCount, winner to Game object; added winningCombinations, checkWinner function to Game object prototype; added scaffolding for board reset, undo, and redo functions

--- app.js
+++ app.js
@@ -10,7 +10,20 @@ $(document).on("ready", function() {
         this.player2 = new Player("O");
         this.board = new Board();
         this.currentMove = "X";
+        this.turnCount = 0;
+        this.winner;
     }
+
+    Game.prototype.winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
 
     // Remember: prototypes are shared functions between all game instances
     Game.prototype.nextPlayer = function() {
@@ -22,10 +35,24 @@ $(document).on("ready", function() {
         //
     };
 
+    Game.prototype.checkWinner = function() {
+        var isWinner = function(symbol) {
+            var hasWon = false;
+            for (var i = 0; i < this.winningCombinations.length; i++) {
+                if (this.board[this.winningCombinations[i][0]] === symbol &&
+                    this.board[this.winningCombinations[i][1]] === symbol &&
+                    this.board[this.winningCombinations[i][2]] === symbol)
+                    hasWon = true;
+            }
+            return hasWon;
+        };
+        if (isWinner("X")) this.winner = "X";
+        if (isWinner("O")) this.winner = "O";
+    };
+
     // A starter Player constructor.
     function Player(team) {
-        //Is the player X or O?
-        //this.team = ...
+        this.team = team;
     }
 
     // A starter Board constructor.
@@ -33,14 +60,27 @@ $(document).on("ready", function() {
         var self = this;
         this.el = $("<div id='board'>");
         this.$cells = {};
-        for (var i=0; i<9; i++) { this.cells.push("<div id='"+i+"' class='box'>&nbsp;</div>"); }
-        this.$cells.forEach(function(v){self.el.append(v);});
-
-        //Tracks the cells of the board instance
-        //this.$cells = ...
+        for (var i = 0; i < 9; i++) {
+            this.$cells.push("<div id='" + i + "' class='box'>&nbsp;</div>");
+        }
+        this.$cells.forEach(function(v) {
+            self.el.append(v);
+        });
 
         //Store any other properties that board may have below, such as a reset option
     }
+
+    Board.prototype.reset() {
+
+    };
+
+    Board.prototype.undo() {
+
+    };
+
+    Board.prototype.redo() {
+
+    };
 
     // Start the game!
     var game = new Game();
