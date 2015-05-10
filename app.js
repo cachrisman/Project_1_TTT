c583b7efabba8527bb9f53c9f5f74e86762420c4
cachrisman <charlie.chrisman@gmail.com>
2015-05-10 14:13:02 -0700

    fixed undo and redo event listener function calls, fixed checkWinner logic, added Board.updateComment function, started implementation of Player object

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ $(document).on("ready", function() {
         this.player1 = new Player("X");
         this.player2 = new Player("O");
         this.board = new Board();
-        this.currentMove = "X";
+        this.currentMove = this.player1.team;
         this.moves = [];
         this.turnCount = 0;
         this.winner = null;
@@ -28,48 +28,49 @@ $(document).on("ready", function() {
 
     // Remember: prototypes are shared functions between all game instances
     Game.prototype.nextPlayer = function() {
-        this.currentMove = this.currentMove === "X" ? "O" : "X";
+        this.currentMove = this.currentMove === this.player1.team ? this.player2.team : this.player1.team;
     };
 
     // `Game.prototype.init` kicks off a new game with a board and two players
     Game.prototype.init = function() {
         this.board.draw();
         $(".box").on("click", this.makeMove.bind(this));
-        $("#undo").on("click", this.board.undoMove); //EL for undo btn
-        $("#redo").on("click", this.board.redoMove); //EL for redo btn
+        $("#undo").on("click", this.board.undo.bind(this)); //EL for undo btn
+        $("#redo").on("click", this.board.redo.bind(this)); //EL for redo btn
     };
 
     Game.prototype.makeMove = function() {
+        $(".box").off("click");
         if (event.target.innerHTML === "X" || event.target.innerHTML === "O") {
-            updateContent("Already selected", "red", "Still " + (XsMove ? "X" : "O") + "'s move", "Already selected. Still " + (XsMove ? "X" : "O") + "'s move");
+            updateContent("Still " + this.currentMove + "'s move", "Already selected", "red", "Already selected. Still " + this.currentMove + "'s move");
         } else {
             event.target.innerHTML = this.currentMove;
             this.board.cells[event.target.id] = this.currentMove;
-            this.moves.push([event.target.id,this.currentMove]);
-            this.nextPlayer();
-            $(".box").off("click");
-            this.board.draw();
-            $(".box").on("click", this.makeMove.bind(this));
-            //updateContent("&nbsp;", "", (XsMove ? "X" : "O") + "'s move");
-            this.turnCount++;
+            this.moves.push([event.target.id, this.currentMove]);
+            this.checkWinner();
+            if (this.winner) {
+                updateContent("&nbsp;", this.currentMove + " Wins!", "lime", this.currentMove + " Wins!");
+            } else {
+                this.nextPlayer();
+                updateContent(this.currentMove + "'s move", "&nbsp;", "", "");
+                this.turnCount++;
+            }
             if (this.turnCount > 0) $('#undo').prop('disabled', false);
-            // this.checkWinner();
         }
+        this.board.draw();
+        $(".box").on("click", this.makeMove.bind(this));
+
     };
 
     Game.prototype.checkWinner = function() {
-        var isWinner = function(symbol) {
-            var hasWon = false;
-            for (var i = 0; i < this.winningCombinations.length; i++) {
-                if (this.board[this.winningCombinations[i][0]] === symbol &&
-                    this.board[this.winningCombinations[i][1]] === symbol &&
-                    this.board[this.winningCombinations[i][2]] === symbol)
-                    hasWon = true;
-            }
-            return hasWon;
-        };
-        if (isWinner("X")) this.winner = "X";
-        if (isWinner("O")) this.winner = "O";
+        var hasWon = false;
+        for (var i = 0; i < this.winningCombinations.length; i++) {
+            if (this.board.cells[this.winningCombinations[i][0]] === this.currentMove &&
+                this.board.cells[this.winningCombinations[i][1]] === this.currentMove &&
+                this.board.cells[this.winningCombinations[i][2]] === this.currentMove)
+                hasWon = true;
+        }
+        if (hasWon) this.winner = this.currentMove;
     };
 
     // A starter Player constructor.
@@ -79,49 +80,41 @@ $(document).on("ready", function() {
 
     // A starter Board constructor.
     function Board() {
-        // var self = this;
         this.cells = [];
-        for (var i = 0; i < 9; i++) {
-            this.cells.push(null);
-        }
-        // this.cells.forEach(function(v) {
-        //     self.el.append(v);
-        // });
+        this.cells = [null, null, null, null, null, null, null, null, null];
         this.playermove_text = "X's move";
         this.notification_text = "&nbsp;";
         this.notification_color = "";
+        this.alert_text = "";
     }
 
     Board.prototype.draw = function() {
+        var $el_board = $("<div id='board'>");
+        for (var i = 0; i < 9; i++) {
+            if (this.cells[i]) $el_board.append("<div id='" + i + "' class='box'>" + this.cells[i] + "</div>");
+            else $el_board.append("<div id='" + i + "' class='box'>&nbsp;</div>");
+        }
+
+        $('#board_container').html("");
+        $('#board_container').append($el_board);
+
         $('#playermove').html(this.playermove_text);
         $('#notification').html(this.notification_text);
         $('#notification').css("background-color", this.notification_color);
+        if (this.alert_text) alert(this.alert_text);
+    };
 
-        var $el_board = $("<div id='board'>");
-        for (var i = 0; i < 9; i++) {
-            if (this.cells[i]) $el_board.append("<div id='"+i+"' class='box'>"+this.cells[i]+"</div>");
-            else $el_board.append("<div id='"+i+"' class='box'>&nbsp;</div>");
-        }
-
-        // var self = this;
-        // $('.box').each( function() {
-        //     $(this).html(self.cells[$(this)[0].id]);
-        // });
-        $('#board_container').html("");
-        $('#board_container').append($el_board);
-        // if (alert_text) alert(alert_text);
-
-
-        // $("#board").off("click");
-        // $("#board").click(makeMove);
-        // if (turnCount > 0) $('#undo').prop('disabled', true);
-        // if (turnCount < this.moves.length) $('#redo').prop('disabled', true);
+    Board.prototype.updateContent = function(playermove_text, notification_text, notification_color, alert_text) {
+        this.playermove_text = playermove_text;
+        this.notification_text = notification_text;
+        this.notification_color = notification_color;
+        this.alert_text = alert_text;
     };
 
     Board.prototype.reset = function() {
         this.currentMove = "X";
         this.turnCount = 0;
-        this.$cells = [null, null, null, null, null, null, null, null, null];
+        this.cells = [null, null, null, null, null, null, null, null, null];
         this.moves = [];
         this.winner = null;
         this.draw();
@@ -131,7 +124,7 @@ $(document).on("ready", function() {
         if (this.turnCount > 0) {
             this.turnCount--;
             this.board[this.moves[this.turnCount][0]] = null;
-            $('#'+this.moves[this.turnCount][0]).html('&nbsp;');
+            $('#' + this.moves[this.turnCount][0]).html('&nbsp;');
             this.currentMove = (this.currentMove === "X" ? "O" : "X");
             this.updateContent("&nbsp;", "", (XsMove ? "X" : "O") + "'s move");
             if (this.turnCount === 0) $('#undo').prop('disabled', true);
@@ -146,7 +139,7 @@ $(document).on("ready", function() {
     Board.prototype.redo = function() {
         if (this.turnCount < this.moves.length) {
             this.board[this.moves[this.turnCount][0]] = this.moves[this.turnCount][1];
-            $('#'+this.moves[this.turnCount][0]).html(this.moves[this.turnCount][1]);
+            $('#' + this.moves[this.turnCount][0]).html(this.moves[this.turnCount][1]);
             this.currentMove = (this.currentMove === "X" ? "O" : "X");
             this.turnCount++;
             if (this.turnCount < this.moves.length) $('#redo').prop('disabled', false);
